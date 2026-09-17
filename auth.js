97ff60a85720254db8cae1599daf91d9354fe9cc
YenNhi2705 <nhi.2474802010282@vanlanguni.vn>
2026-09-17 14:06:57 +0700

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-function login(username, password) {
-  return username === "admin" && password === "123";
-}
-
-// Dùng cho Jest trong môi trường Node.js
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = login;
-}
